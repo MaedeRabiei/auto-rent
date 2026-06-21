176e5a4d87f3e6301a43c2625ff3fa68075537f3
maede-28mra <maederabiei09@gmail.com>
2026-06-22 00:27:17 +0330

    magazine

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const commentsSwiper = new Swiper(".comments-s", {
     spaceBetween: 20,
     centeredSlides: true,
 
-   
+
 
     breakpoints: {
         0: {
@@ -46,9 +46,37 @@ const commentsSwiper = new Swiper(".comments-s", {
     },
     speed: 800,
 
-     navigation: {
+    navigation: {
         nextEl: ".comments-next",
         prevEl: ".comments-prev",
+    },
+
+});
+const magazineSwiper = new Swiper(".magazine-s", {
+    spaceBetween: 40,
+    centeredSlides: true,
+
+
+
+    breakpoints: {
+        0: {
+            slidesPerView:2,
+        },
+
+        768: {
+            slidesPerView: 3,
+            centeredSlides: false,
+        },
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 800,
+
+    navigation: {
+        nextEl: ".magazine-prev",
+        prevEl: ".magazine-next",
     },
 
 });
